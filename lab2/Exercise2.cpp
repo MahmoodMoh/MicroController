88dd0b311e1b9d5528b8305177ce66b6b9bb16df
MahmoodMoh <mahmoodmohammadi050@gmail.com>
2025-04-08 18:54:30 +0300

    Excercise 2

--- lab2/Exercise2.cpp
+++ lab2/Exercise2.cpp
@@ -1,24 +1,34 @@
 #include <iostream>
 using namespace std;
 
-int main(){
-    //Method 1
-    /*int a = 5;
-    int b = 10;
+// Swap using pointers
+void swapUsingPointers(float* a, float* b) {
+    float temp = *a;
+    *a = *b;
+    *b = temp;
+}
 
-    int temp = a;
+// Swap using references
+void swapUsingReferences(float &a, float &b) {
+    float temp = a;
     a = b;
     b = temp;
-    cout<<"a = " <<a<< "b = " << b << endl;*/
+}
 
-    //Method 2
-    int a = 5, b = 10;
-    a = a + b; // 15
-    b = a - b; // 15 - 10 = 5
-    a = a - b; // 15 - 5 = 10
+int main() {
+    float x = 10.5, y = 20.0;
+    float temp = x;
+    x = y;
+    y = temp;
+    cout <<"x = " << x << endl;
+    cout <<" y = " << y << endl;
 
-    cout <<"a = " << a << endl;
-    cout <<"b = " << b << endl;
+    cout << "Before pointer swap: x = " << x << ", y = " << y << endl;
+    swapUsingPointers(&x, &y); // pass addresses
+    cout << "After pointer swap: x = " << x << ", y = " << y << endl;
+
+    swapUsingReferences(x, y); // pass by reference
+    cout << "After reference swap: x = " << x << ", y = " << y << endl;
 
     return 0;
 }
